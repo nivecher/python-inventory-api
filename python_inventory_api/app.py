65d510b0e8b71218e2b50ce39dd9c9f7282f37a3
nivecher <nivecher@users.noreply.github.com>
2023-07-28 23:24:50 +0000

    ✅ Ready to clone and code.

--- python_inventory_api/app.py
+++ python_inventory_api/app.py
@@ -23,18 +23,18 @@ def read(*paths, **kwargs):
 
 
 description = """
-project_name API helps you do awesome stuff. 🚀
+python_inventory_api API helps you do awesome stuff. 🚀
 """
 
 app = FastAPI(
-    title="project_name",
+    title="python_inventory_api",
     description=description,
     version=read("VERSION"),
-    terms_of_service="http://project_name.com/terms/",
+    terms_of_service="http://python_inventory_api.com/terms/",
     contact={
-        "name": "author_name",
-        "url": "http://project_name.com/contact/",
-        "email": "author_name@project_name.com",
+        "name": "nivecher",
+        "url": "http://python_inventory_api.com/contact/",
+        "email": "nivecher@python_inventory_api.com",
     },
     license_info={
         "name": "The Unlicense",
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""Python setup.py for project_name package"""
+"""Python setup.py for python_inventory_api package"""
 import io
 import os
 from setuptools import find_packages, setup
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 def read(*paths, **kwargs):
     """Read the contents of a text file safely.
-    >>> read("project_name", "VERSION")
+    >>> read("python_inventory_api", "VERSION")
     '0.1.0'
     >>> read("README.md")
     ...
@@ -30,17 +30,17 @@ def read_requirements(path):
 
 
 setup(
-    name="project_name",
-    version=read("project_name", "VERSION"),
-    description="project_description",
-    url="https://github.com/author_name/project_urlname/",
+    name="python_inventory_api",
+    version=read("python_inventory_api", "VERSION"),
+    description="Awesome python_inventory_api created by nivecher",
+    url="https://github.com/nivecher/python-inventory-api/",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    author="author_name",
+    author="nivecher",
     packages=find_packages(exclude=["tests", ".github"]),
     install_requires=read_requirements("requirements.txt"),
     entry_points={
-        "console_scripts": ["project_name = project_name.__main__:main"]
+        "console_scripts": ["python_inventory_api = python_inventory_api.__main__:main"]
     },
     extras_require={"test": read_requirements("requirements-test.txt")},
 )
